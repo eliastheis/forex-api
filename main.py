20352df0d9d4ce84fb78b6fe3170d1092891ac76
eliastheis <eliastheis.sg@gmail.com>
2019-12-25 22:01:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import util
 import csv
 import os
 from matplotlib import pyplot as plt
-from random import randint
 from datetime import datetime
 import time
 import pytz
 from dateutil import parser
+import requests
+import json
 
-symbol = 'eurusd'
+symbol = 'eurusd' # format 'eurusd'
+symbol_upper = 'EUR/USD' # format 'EUR/USD'
 
 def timestamp_from_date(date):
 	return int(parser.parse(date + ' UTC').timestamp()*1000)
@@ -17,7 +19,18 @@ def date_from_timestamp(timestamp):
 def rreplace(s, old, new, occurrence):
 	li = s.rsplit(old, occurrence)
 	return new.join(li)
-
+def getPrice(symbol, old_price):
+	try:
+		data = requests.get('https://fcsapi.com/api/forex/latest?symbol=' + symbol + '&access_key=ulz8KI9wbwTVY2b2BBtUjhoJFDFhyndoqOpmvXqbLuTDGOV9Vu').text
+		data = json.loads(data)
+		if data['status'] == True:
+			return float(data['response'][0]['price'])
+		else:
+			return old_price
+	except Exception as e:
+		print('## EXCEPTION ##')
+		print(e)
+		return old_price
 # connect to SQL
 host, user, password, database = util.getSqlConfig()
 util.connectToSQL(host, user, password, database)
@@ -30,7 +43,7 @@ plt.ion()
 data = []
 
 # read files
-for file in files:
+for file in ['2019-now.csv']:#files:
 	with open('data/' + file) as f:
 		csv_reader = csv.reader(f)
 		next(csv_reader)
@@ -63,17 +76,53 @@ util.executeSQL("TRUNCATE TABLE " + symbol)
 
 current = 0
 counter = 0
-limit = 1000
+limit = 10000
 cmd = ""
 base = "INSERT INTO " + symbol + " (`timestamp`, `open`, `high`, `low`, `close`, `volume`) VALUES "
+all_timestamps = []
+tmp = False
 for i in range(len(data)):
 	d = data[i]
-	cmd += "('" + str(d[0]) + "', '" + str(d[1]) + "', '" + str(d[2]) + "', '" + str(d[3]) + "', '" + str(d[4]) + "', '" + str(d[5]) + "'),"
-	counter += 1
-	if counter == limit:
-		counter = 0
-		util.executeSQL(base + rreplace(cmd, ',', ';', 1))
-		cmd = ""
+	if d[0] not in all_timestamps:
+		all_timestamps.append(d[0])
+		cmd += "('" + str(d[0]) + "', '" + str(d[1]) + "', '" + str(d[2]) + "', '" + str(d[3]) + "', '" + str(d[4]) + "', '" + str(d[5]) + "'),"
+		counter += 1
+		if counter == limit:
+			counter = 0
+			util.executeSQL(base + rreplace(cmd, ',', ';', 1))
+			cmd = ""
+			all_timestamps = all_timestamps[len(all_timestamps)-87000:]
 	if round((100*i)/len(data)) != current:
 		current = round(100*i/len(data))
-		print(str(current) + '%')
+		util.p('LOADS', str(current) + '%')
+util.executeSQL(base + rreplace(cmd, ',', ';', 1))
+
+# get live data from fcsapi.com
+open = data[-1][4]
+high = open
+low = open
+close = open
+last_ms = data[-1][0]
+time_step = 60000 # one minute
+
+next_update_ms = last_ms + time_step
+
+while True:
+	
+	# check for update
+	if int(round(time.time() * 1000)) > next_update_ms:
+		util.p('PRICE', date_from_timestamp(next_update_ms) + ' ' + str(open) + ' ' + str(high) + ' ' + str(low) + ' ' + str(close))
+		util.executeSQL(base + "('" + str(next_update_ms) + "', '" + str(open) + "', '" + str(high) + "', '" + str(low) + "', '" + str(close) + "', '" + str(1) + "');")
+		next_update_ms += time_step
+		open = close
+		high = close
+		low = close
+	
+	# update price
+	close = getPrice(symbol_upper, close)
+	if close > high:
+		high = close
+	if close < low:
+		low = close
+	
+	time.sleep(6)
